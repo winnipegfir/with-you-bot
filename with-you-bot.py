73e0e835f7f316de910a8331b45edc779d0ec593
kolbyd <kolby.dunning@gmail.com>
2020-09-05 17:51:00 -0400

    Database adding

--- with-you-bot.py
+++ with-you-bot.py
@@ -5,20 +5,20 @@ from dotenv import load_dotenv
 
 import datetime
 
+import mysql.connector
+
 print("Starting...")
 
+#Make sure we have our required files
 if os.path.exists('.env'):
+    load_dotenv()
     pass
 else:
     f = open('.env', 'x')
-    f.write("# .env\nDISCORD_TOKEN=\nADMIN_ID=")
+    f.write("# .env\nDISCORD_TOKEN=\nADMIN_ID=\n\n# Database stuff\nDB_HOST=\nDB_USER=\nDB_PASSWORD=\nDB_DATABASE=")
     f.close()
-
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-ADMIN = int(os.getenv('ADMIN_ID'))
-
-client = discord.Client()
+    print("You need to fill in the fields in the .env file.")
+    exit()
 
 if os.path.exists('counter.txt'):
     pass
@@ -34,20 +34,28 @@ else:
     f.write("0")
     f.close()
 
+TOKEN = os.getenv('DISCORD_TOKEN')
+ADMIN = int(os.getenv('ADMIN_ID'))
 
+database = mysql.connector.connect(
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    host=os.getenv("DB_HOST"),
+    database=os.getenv("DB_DATABASE")
+)
+
+client = discord.Client()
 @client.event
 async def on_ready():
     activity = discord.Activity(name="\"with you\"s, sadly", type=discord.ActivityType.listening)
     await client.change_presence(activity=activity)
     print("Bot is ready!")
 
-
 @client.event
 async def on_message(message):
     channel = message.channel
     utc_datetime = datetime.datetime.utcnow()
     # .withyou
-    global counter
     if message.content.lower() == (".withyou"):
         # Read the file, add one, read again
         with open('./counter.txt', 'r') as file:
@@ -56,6 +64,17 @@ async def on_message(message):
             file.write(str(int(counter) + 1))
         with open('./counter.txt', 'r') as file:
             counter = file.read()
+
+        write_database = database.cursor(buffered=True)
+        write_database.execute("SELECT * FROM withyou WHERE discord_id = " + str(message.author.id))
+        exists = write_database.fetchone()
+
+        if(exists):
+            write_database.execute("UPDATE withyou SET times = times + 1 WHERE discord_id = " + str(message.author.id))
+        else:
+            write_database.execute("INSERT INTO withyou (discord_id, times) VALUES (" + str(message.author.id) + ", 1)")
+
+        database.commit()
 
         # We've got our magical number, send the message!
         embed = discord.Embed(title=("\"With You\" Bot"), type="Rich", description="The \"with you\":tm: counter is now at " + str(counter) + ".", color=0x013162)
@@ -71,6 +90,10 @@ async def on_message(message):
         with open('./counter.txt', 'r') as file:
             counter = file.read()
 
+        write_database = database.cursor(buffered=True)
+        write_database.execute("UPDATE withyou SET times = times - 1 WHERE discord_id = " + str(message.author.id))
+        database.commit()
+
         # We've got our magical number, send the message!
         embed = discord.Embed(title=("\"With You\" Bot"), type="Rich", description="A \"with you\" has been removed. The \"with you\":tm: counter is now at " + str(counter) + ".", color=0x013162)
         embed.set_footer(text=("This \"with you\" was removed at " + str(utc_datetime.strftime("%H%Mz")) + " | © Kolby Dunning"))
@@ -85,6 +108,7 @@ async def on_message(message):
         embed = discord.Embed(title=("\"With You\" Bot"), type="Rich", description="The \"with you\":tm: counter is at " + str(counter) + ".", color=0x013162)
         embed.set_footer(text=("The \"with you\" counter was viewed at " + str(utc_datetime.strftime("%H%Mz")) + " | © Kolby Dunning"))
         await channel.send(embed=embed)
+
     if message.content.lower() == (".withyou help"):
         embed = discord.Embed(title=("\"With You\" Bot"), type="Rich", description="The \"with you\":tm: bot is for \"with you\" check in's by pilots on frequency. Feel free to add one to the counter if you hear one!", color=0x013162)
         embed.add_field(name=".withyou", value="Adds 1 \"with you\" to the counter!", inline=False)
@@ -103,28 +127,24 @@ async def on_message(message):
             return
         else:
             user = client.get_user(ADMIN)
-            new = message.content[13:]
+            new_number = message.content[13:]
             try:
-                int(new)
+                int(new_number)
                 pass
             except ValueError:
-                embed = discord.Embed(title=("\"With You\" Bot"), type="Rich",
-                                      description="Uh oh. The number need to be an integer.", color=0xFF0000)
-                embed.set_footer(text=("The \"with you\" counter was set at " + str(
-                    utc_datetime.strftime("%H%Mz")) + " | © Kolby Dunning"))
+                embed = discord.Embed(title=("\"With You\" Bot"), type="Rich", description="Uh oh. The number need to be an integer.", color=0xFF0000)
+                embed.set_footer(text=("The \"with you\" counter was set at " + str(utc_datetime.strftime("%H%Mz")) + " | © Kolby Dunning"))
                 await user.send(embed=embed)
                 return
 
             # Overwrite the current number with the specified
             with open('./counter.txt', 'w') as file:
-                file.write(new)
+                file.write(new_number)
             with open('./counter.txt', 'r') as file:
                 counter = file.read()
 
             # Tell me so I know it works!
-            embed = discord.Embed(title=("\"With You\" Bot"), type="Rich",
-                                  description="The \"with you\":tm: counter has been set at " + str(counter) + ".",
-                                  color=0x013162)
+            embed = discord.Embed(title=("\"With You\" Bot"), type="Rich", description="The \"with you\":tm: counter has been set at " + str(counter) + ".", color=0x013162)
             embed.set_footer(text=("The \"with you\" counter was set at " + str(
                 utc_datetime.strftime("%H%Mz")) + " | © Kolby Dunning"))
             await user.send(embed=embed)
